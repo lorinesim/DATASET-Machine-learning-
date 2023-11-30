de998b4fd557c4653434255bae94ffeba81f7df6
lorinesim <lorinesin02@gmail.com>
2023-11-30 18:38:57 +0300

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -1,4 +1,3 @@
-# Import packages needed
 import os
 import numpy as np
 import pandas as pd
@@ -6,63 +5,50 @@ from sklearn import svm
 from sklearn import preprocessing
 import time
 
-# This function builds training dataset and testing dataset
+# Bu fonksiyon,  output.csv dosyasından veri setini çeker
 def Build_Data_Set():
-	df = pd.read_csv('../Data/out.csv', index_col=0) 	# 100000 entries
-	df_train = df[:50000] 	# Take last 50000 rows as a training dataset
-	df_test = df[10000:]   # Take first 10000 rows as a testing dataset
+    
+    df = pd.read_csv('output.csv', index_col=0)   
+    df_train = df[:50000]  # Son 50000 satırı eğitim veri seti olarak kullan
+    df_test = df[10000:]  # İlk 10000 satırı test veri seti olarak kullan
 
-	# Clean up training dataset and scale it
-	X_train = np.array(df_train.drop(['classification','usage_counter', 'normal_prio', 'policy', 'vm_pgoff', 'task_size', 'cached_hole_size', 'hiwater_rss', 'nr_ptes', 'lock', 'cgtime', 'signal_nvcsw'], 1))
-	X_train = preprocessing.scale(X_train)
-	# Training label
-	y_train = np.array(df_train['classification'].replace("malware",0).replace("benign",1))
+    # Eğitim veri setini temizle ve ölçeklendir
+    X_train = np.array(df_train.drop(['classification', 'usage_counter', 'normal_prio', 'policy', 'vm_pgoff', 'task_size', 'cached_hole_size', 'hiwater_rss', 'nr_ptes', 'lock', 'cgtime', 'signal_nvcsw'], 1))
+    X_train = preprocessing.scale(X_train)
+    # Eğitim etiketi
+    y_train = np.array(df_train['classification'].replace("malware", 0).replace("benign", 1))
 
-	# Clean up testing dataset and scale it
-	X_test = np.array(df_test.drop(['classification','usage_counter', 'normal_prio', 'policy', 'vm_pgoff', 'task_size', 'cached_hole_size', 'hiwater_rss', 'nr_ptes', 'lock', 'cgtime', 'signal_nvcsw'], 1))
-	X_test = preprocessing.scale(X_test)
-	#Testing label
-	y_test = np.array(df_test['classification'].replace("malware",0).replace("benign",1))
+    # Test veri setini temizle ve ölçeklendir
+    X_test = np.array(df_test.drop(['classification', 'usage_counter', 'normal_prio', 'policy', 'vm_pgoff', 'task_size', 'cached_hole_size', 'hiwater_rss', 'nr_ptes', 'lock', 'cgtime', 'signal_nvcsw'], 1))
+    X_test = preprocessing.scale(X_test)
+    # Test etiketi
+    y_test = np.array(df_test['classification'].replace("malware", 0).replace("benign", 1))
 
-	return X_train, X_test, y_train, y_test # Return arrays
+    return X_train, X_test, y_train, y_test  # Dizileri döndür
 
-# This function builds a machine learning model using scikit-learn svm algorithm and compute the Accuracy of the prediction
+# Bu fonksiyon, kendi veri setinizle bir makine öğrenimi modeli oluşturur ve doğruluk oranını hesaplar
 def Analysis():
-	test_size = 10000 # The size of the testing dataset
-	X_train, X_test, y_train, y_test = Build_Data_Set() # Building training and testing datasets
+    test_size = 10000  # Test veri setinin boyutu
+    X_train, X_test, y_train, y_test = Build_Data_Set()  # Eğitim ve test veri setlerini oluştur
 
-	clf = svm.SVC(kernel="linear", C=0.01) # Declare a svm object in with scikit-learn package 
-	clf.fit(X_train, y_train) # Training the model with the traning dataset and labels
-	result = clf.predict(X_test) # Running a prediction with 10000 samples
+    clf = svm.SVC(kernel="linear", C=0.01)  # Scikit-learn paketi ile bir svm nesnesi oluştur
+    clf.fit(X_train, y_train)  # Modeli eğitim veri seti ve etiketleriyle eğit
+    result = clf.predict(X_test)  # 10000 örnek üzerinde bir tahmin çalıştır
 
-	# Compute the accuracy and print it out
-	correct_count = 0
-	for i in range(0,test_size):
-		if result[i] == y_test[i]:
-			correct_count += 1
+    # Doğruluk oranını hesapla ve yazdır
+    correct_count = 0
+    for i in range(0, test_size):
+        if result[i] == y_test[i]:
+            correct_count += 1
 
-	print("Accuracy:", (correct_count/test_size)*100)
-	return
-
+    print("Accuracy:", (correct_count / test_size) * 100)
+    return
 
 start = time.time()
-Analysis() # run the program
+Analysis()  # Programı çalıştır
 end = time.time()
 
 elapsed = end - start
 
-print("Time:",elapsed)
+print("Time:", elapsed)
 
-
-# import sha3
-# import hashlib
-# # encoding GeeksforGeeks using md5 hash
-# # function 
-# df = pd.read_csv('new_data.csv', index_col=0) 	# 100000 entries
-
-# # s = hashlib.sha3_512()
-# # s.update(b"hello")
-# # print(s.hexdigest())
-
-# df['hash'] = df['hash'].apply(hash)
-# df.to_csv('out.csv')
